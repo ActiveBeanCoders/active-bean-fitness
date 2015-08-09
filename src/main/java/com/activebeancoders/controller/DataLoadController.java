51f6059d3b1567980421d07101cefa962531fd44
Dan Barrese <danielbarrese@gmail.com>
2015-08-09 06:11:44 -0600

    Made index rebuild asynchronous.

--- src/main/java/com/activebeancoders/controller/DataLoadController.java
+++ src/main/java/com/activebeancoders/controller/DataLoadController.java
@@ -1,10 +1,14 @@
 package com.activebeancoders.controller;
 
 import com.activebeancoders.service.EsIndexer;
+import com.google.common.util.concurrent.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @RestController
 public class DataLoadController {
@@ -13,14 +17,41 @@ public class DataLoadController {
 
     @Autowired
     private EsIndexer esIndexer;
+    private String lastKnownStatus = "Inactive.";
 
     /**
      * Rebilds the index with random data.
      */
     @RequestMapping(value = RestEndpoint.RELOAD, method = RequestMethod.GET)
-    public void reloadActivities() {
-        esIndexer.rebuildAllIndexStructures();
-        esIndexer.indexAllData();
+    public String reloadActivities() {
+        ListeningExecutorService executorService = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
+        Runnable runnable = () -> {
+            System.out.println("a");
+            esIndexer.rebuildAllIndexStructures();
+            System.out.println("b");
+            esIndexer.indexAllData();
+            System.out.println("c");
+        };
+        ListenableFuture<?> future = executorService.submit(runnable);
+        Futures.addCallback(future, new FutureCallback<Object>() {
+            @Override
+            public void onSuccess(Object o) {
+                log.info("Data reload complete.");
+                lastKnownStatus = "Data reload complete.";
+            }
+            @Override
+            public void onFailure(Throwable t) {
+                log.error("Failed to reload data.", t);
+                lastKnownStatus = "Failed to reload data";
+            }
+        });
+        executorService.shutdown();
+        return "Okay, I'm working on it!";
+    }
+
+    @RequestMapping(value = RestEndpoint.RELOAD_STATUS, method = RequestMethod.GET)
+    public String reloadActivitiesStatus() {
+        return lastKnownStatus;
     }
 
 }
--- src/main/java/com/activebeancoders/controller/RestEndpoint.java
+++ src/main/java/com/activebeancoders/controller/RestEndpoint.java
@@ -6,5 +6,6 @@ public class RestEndpoint {
     public static final String ACTIVITY_LOG = "/activityLog";
     public static final String RESOURCE = "/resource";
     public static final String RELOAD = "/reload";
+    public static final String RELOAD_STATUS = "/reload-status";
 
 }
