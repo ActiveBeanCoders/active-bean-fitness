15ee9f4cff638931eed8848b8c0caabcd73adfee
Dan Barrese <DBarrese@trustwave.com>
2015-09-10 00:03:55 -0600

    removed unused import

--- src/main/java/com/activebeancoders/Config.java
+++ src/main/java/com/activebeancoders/Config.java
@@ -4,7 +4,6 @@ import com.activebeancoders.controller.es.ActivityController;
 import com.activebeancoders.controller.es.DataLoadController;
 import com.activebeancoders.dao.es.ActivityEsDao;
 import com.activebeancoders.service.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
