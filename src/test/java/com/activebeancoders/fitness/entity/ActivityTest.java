f08735128654ed64b16f2d0c4c9721fa3b7fe618
Dan Barrese <DBarrese@trustwave.com>
2015-09-20 21:19:59 -0600

    test

---
+++ src/test/java/com/activebeancoders/fitness/entity/ActivityTest.java
@@ -0,0 +1,20 @@
+package com.activebeancoders.fitness.entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActivityTest {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void testStringSplitting() throws Exception {
+        Activity activity = new Activity();
+        activity.setComment("ab-c \"def\": gh, i. J! *k^lmnop");
+        System.out.println(activity.getAllText());
+        Assert.assertEquals("def lmnop", activity.getAllText());
+    }
+
+}
