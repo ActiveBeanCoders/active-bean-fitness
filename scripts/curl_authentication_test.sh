96da5a8f407476252dc78f5fc6121fda7c248dab
Dan Barrese <danielbarrese@gmail.com>
2015-11-18 22:38:34 -0700

    updated script

--- scripts/curl_authentication_test.sh
+++ scripts/curl_authentication_test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-token=$(curl -s -XPOST --header "X-Auth-Username: user" --header "X-Auth-Password: password" http://localhost:8080/security/public/authenticate) && printf '%s\n' "session_token=${token}" && curl -XGET --header "X-Auth-Token: ${token}" http://localhost:8080/resource/activity/1 && printf '\n'
+function die {
+    printf '%s\n' "Dying..."
+    exit 1
+}
+token=$(curl -s -XPOST --header "X-Auth-Username: user" --header "X-Auth-Password: password" http://localhost:8080/security/public/authenticate) || die
+if [[ ${token} == *token* ]]; then
+    token=$(echo ${token} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["token"]')
+fi
+printf '%s\n' "session_token=${token}"
+curl -XGET --header "X-Auth-Token: ${token}" http://localhost:8080/resource/activity/1 && printf '\n'
 
