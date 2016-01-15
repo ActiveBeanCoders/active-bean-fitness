a92f09ae45b1f9addd0be38d90185bf0f1ddb372
Dan Barrese <danielbarrese@gmail.com>
2016-01-14 19:24:28 -0700

    more stuff!

--- data-access-service/src/integration_test/java/com/activebeancoders/fitness/RemotingTestIT.java
+++ data-access-service/src/integration_test/java/com/activebeancoders/fitness/RemotingTestIT.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,7 +13,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Dan Barrese
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {DataAccessServiceApplication.class})
+@ContextConfiguration(classes = DataAccessServiceApplication.class)
+@WebIntegrationTest // Make sure app is NOT running outside of your IDE!
 public class RemotingTestIT {
 
     @Autowired
